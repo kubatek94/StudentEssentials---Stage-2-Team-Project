b8df2805ae052e7f3de20e02308f1d7fa3c05cb5
kubatek94 <j.gawron@newcastle.ac.uk>
2013-12-04 15:53:02 +0000

    Added author information

--- src/com/example/studentessentials/AuthenticationException.java
+++ src/com/example/studentessentials/AuthenticationException.java
@@ -1,5 +1,10 @@
 package com.example.studentessentials;
 
+/**
+ * Author: Jakub Gawron
+ * Date: 03.12.2013
+ */
+
 public class AuthenticationException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
